4dddffa85e9261713c0d81a253107b59eacaecdf
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-04-16 00:04:22 +0600

    Create Problem-1016.c

---
+++ Programming language C/Problem-1016.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int A,result;
+    scanf("%d",&A);
+
+    const int X = 60;
+    const int Y = 90;
+    
+    result = 2*A;
+    printf("%d minutos\n",result);
+    
+    return 0;
+}
