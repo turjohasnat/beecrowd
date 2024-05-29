b8238145287babc688d0ae1a8f67b6cfd57a5f1b
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-29 11:18:33 +0600

    Create Solution-1008.c

---
+++ Programming language C/Problem-1008/Solution-1008.c
@@ -0,0 +1,13 @@
+//C Solution:
+#include<stdio.h>
+
+int main(){
+    int total_employee;
+    float total_hour, per_hour;
+    scanf("%d",&total_employee);
+    scanf("%f",&total_hour);
+    scanf("%f",&per_hour);
+    printf("NUMBER = %d\n",total_employee);
+    printf("SALARY = U$ %.2f\n",(total_hour*per_hour));
+    return 0;
+}
