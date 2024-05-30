ee08ab0b3e9f82962d28aac91af9aaf1657d184b
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-30 11:29:32 +0600

    Create Solution-1013.c

---
+++ Programming language C/Problem-1013/Solution-1013.c
@@ -0,0 +1,19 @@
+//C Solution:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  
+    int a,b,c, greatest;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+
+    greatest = (a + b + abs(a-b))/2;
+    greatest = (greatest + c + abs(greatest-c))/2;
+
+    printf("%d eh o maior\n",greatest);
+    
+    return 0;
+}
