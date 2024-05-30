ecfe805ff258cc18001a6f01dbaf8dfcd0860bb2
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-30 11:33:47 +0600

    Create Solution-1016.c

---
+++ Programming language C/Problem-1016/Solution-1016.c
@@ -0,0 +1,15 @@
+//C Solution:
+#include<stdio.h>
+
+int main(){
+    int A,result;
+    scanf("%d",&A);
+
+    const int X = 60;
+    const int Y = 90;
+    
+    result = 2*A;
+    printf("%d minutos\n",result);
+    
+    return 0;
+}
