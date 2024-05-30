3b6fc2f0899f94a52c25a37d7c949523cf9fea78
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-05-30 11:31:12 +0600

    Create Solution-1014.c

---
+++ Programming language C/Problem-1014/Solution-1014.c
@@ -0,0 +1,16 @@
+//C Solution:
+#include<stdio.h>
+
+int main(){
+    int total_distance;
+    scanf("%d",&total_distance);
+
+    float total_fuel,average_consumption;
+    scanf("%f",&total_fuel);
+
+    average_consumption = total_distance/total_fuel;
+
+    printf("%.3f km/l\n",average_consumption);
+    
+    return 0;
+}
