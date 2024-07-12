66427320201abf1d46e75104d8a617c5effd13ed
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-07-12 22:25:30 +0600

    Create Solution-1053.c

---
+++ Programming language C/Problem-1053/Solution-1053.c
@@ -0,0 +1,12 @@
+//C Solution:
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int fact = 1;
+    for ( int i = 1; i <= n; i++ ){
+        fact *= i;
+    }
+    printf("%d\n", fact);
+    return 0;
+}
