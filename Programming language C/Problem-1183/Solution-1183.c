f583ff8f6b53b29c55ff0912331483c90c15e573
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-08-08 11:43:50 +0600

    Create Solution-1183.c

---
+++ Programming language C/Problem-1183/Solution-1183.c
@@ -0,0 +1,23 @@
+//C Solution:
+#include<stdio.h>
+int main(){
+    double m[12][12], sum = 0.0;
+    char t[2];
+    scanf("%c", &t);
+    for ( int i = 0; i < 12; i++ ){
+        for ( int j = 0; j < 12; j++ ){
+            scanf("%lf", &m[i][j]);
+        }
+    }
+    int p = 1;
+    for ( int i = 0; i < 12; i++ ){
+        for ( int j = p; j < 12; j++ )
+            sum += m[i][j];
+        p++;
+    }
+    
+    if ( t[0] == 'S' ) printf("%.1lf\n", sum);
+    else if ( t[0] == 'M' ) printf("%.1lf\n", sum/66.0 );
+
+    return 0;
+}
